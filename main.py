573d967b2b5bf87022ebeb51cc8721f86ddb0ce4
Troshin-Egor-py <145129273+Troshin-Egor-py@users.noreply.github.com>
2024-03-06 18:43:17 +0300

    tr

--- main.py
+++ main.py
@@ -3,4 +3,27 @@ import random
 
 
 SIZE = 500
-pygame.display.set_mode((SIZE, SIZE))
+FPS = 1
+GAME = True
+CLOCK = pygame.time.Clock()
+WINDOW = pygame.display.set_mode((SIZE, SIZE))
+
+
+
+
+
+while GAME:
+    COLOR = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    WINDOW.fill(COLOR)
+
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                exit()
+
+
+    pygame.display.update()
+    CLOCK.tick(FPS)
